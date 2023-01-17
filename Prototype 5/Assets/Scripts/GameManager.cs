6c69c5d7c7347ac647014e1bd324a7adb25e928c
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2023-01-17 21:06:29 +0800

    add a game over mechanic, triggered when a good target is missed, add a restart mechanic, triggered when clicking restart button that shows when game is over

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public bool isGameAcitve = true;
     public int score = 0;
     private float spawnRate = 1.0f;
 
@@ -14,22 +19,25 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        isGameAcitve = true;
+        score = 0;
         StartCoroutine(SpawnTarget());
         UpdateScore(score);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     IEnumerator SpawnTarget()
     {
-        while (true)
-        { 
+        while (isGameAcitve)
+        {
             yield return new WaitForSeconds(spawnRate);
-            int index = Random.Range(0, targets.Count); 
+            int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
 
         }
@@ -40,4 +48,18 @@ public class GameManager : MonoBehaviour
         score += scoreToAdd;
         scoreText.text = $"Score: {score}";
     }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameAcitve = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
 }
