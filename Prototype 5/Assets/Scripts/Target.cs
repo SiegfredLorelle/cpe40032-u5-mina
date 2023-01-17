936af9dfe4860c39523204e30a299af262aeee85
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2023-01-17 14:28:37 +0800

    spawn prefabs upward, clicking destroys them

--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody targetRb;
+    private float minSpeed = 12.0f;
+    private float maxSpeed = 16.0f;
+    private float maxTorque = 10.0f;
+    private float xRange = 4.0f;
+    private float spawnPosY = -6.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpanwPos();
+    
     }
 
     // Update is called once per frame
@@ -15,4 +26,30 @@ public class Target : MonoBehaviour
     {
         
     }
-}
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpanwPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), spawnPosY);
+    }
+
+}   
