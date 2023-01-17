e67bcac6a0e01bdfc5a82d09c9ff488bc2c8af01
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2023-01-17 16:14:20 +0800

    add a tmp text showing current score, score is updated every target clicked, show particle effects when clicking targets

--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private GameManager gameManager; 
     private Rigidbody targetRb;
+
     private float minSpeed = 12.0f;
     private float maxSpeed = 16.0f;
     private float maxTorque = 10.0f;
     private float xRange = 4.0f;
-    private float spawnPosY = -6.0f;
+    private float spawnPosY = -2.0f;
+
+    public ParticleSystem explosionParticle;
+    public int pointValue;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         targetRb = GetComponent<Rigidbody>();
+
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpanwPos();
@@ -30,6 +37,9 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+
     }
 
     private void OnTriggerEnter(Collider other)
